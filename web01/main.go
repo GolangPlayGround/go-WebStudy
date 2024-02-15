391d3bb6dcf895c3c81aefe71c6ade23f139f2a6
Junseong kim <anima94@kakao.com>
2024-02-15 19:52:03 +0900

    study : chap02 json transfer

--- web01/main.go
+++ web01/main.go
@@ -1,26 +1,54 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 )
 
 type fooHandler struct{}
 
+type User struct {
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Email     string
+	CreatedAt time.Time
+}
+
 func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, "Hello World")
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Bad Request: ", err)
+		return
+	}
+	user.CreatedAt = time.Now()
+	data, _ := json.Marshal(user)
+	w.Header().Add("content-type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	fmt.Fprint(w, string(data))
+}
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!", name)
 }
 
 func main() {
-	http.HandleFunc("/", func(w http.ResponseWriter, request *http.Request) {
+	mux := http.NewServeMux() // http.ServeMux 타입의 인스턴스 반환. (이 인스턴스를 사용해 특정 URL패턴에 대한 핸들러 함수 등록 가능)
+
+	mux.HandleFunc("/", func(w http.ResponseWriter, request *http.Request) {
 		fmt.Fprint(w, "Hello World")
 	})
 
-	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprint(w, "Hello Bar!")
-	})
+	mux.HandleFunc("/bar", barHandler)
 
-	http.Handle("/foo", &fooHandler{})
+	mux.Handle("/foo", &fooHandler{})
 
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", mux)
 }
